a1ac1c5f4d0b8a5948bf759fc7c3fbbe06008208
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2022-09-21 15:05:05 +1000

    Refactor variable name.

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -4,9 +4,10 @@
 # Price of item: 3.24
 # Total price for 3 items is $124.92
 
+DISCOUNT_RATE = 0.9
+
 items = int(input("Number of items: "))
 total_price = 0
-discount = 0.9
 while items <= 0:
     print("Invalid item amounts, please enter a valid amount")
     items = float(input("Number of items: "))
@@ -16,10 +17,8 @@ for i in range(items):
     total_price += item_price
 
 if total_price > 100:
-    discount_price = discount * total_price
+    discount_price = DISCOUNT_RATE * total_price
     print("Total price for {} items is ${:.2f} with a 10% discount"
           .format(items, discount_price))
 else:
     print("Total price for {} items is ${:.2f}".format(items, total_price))
-
-
