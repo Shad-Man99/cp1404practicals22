19f195d685134525dc9b410325bafb3cb6590854
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2022-09-23 20:14:47 +1000

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,9 @@
 """Test"""
 
+min_length = 10
 password = input("Enter password: ")
+while len(password) < min_length:
+    print("Invalid password length, enter a longer password")
+    password = input("Enter password: ")
+else:
+    print("*" * len(password))
