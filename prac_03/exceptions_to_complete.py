8c486d24476a4205d2e2ddb2ba14bb2ddd145863
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2022-10-02 13:20:49 +1000

    Changed print format to f-string.

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -9,7 +9,7 @@ while not is_finished:
         result = int(input("Enter a valid integer: "))
         is_finished = True
     except ValueError:
-        print("Please enter a valid integer.")
-print("Valid result is:", result)
+        print(f"Please enter a valid integer.")
+print(f"Valid result is:{result}")
 
 
