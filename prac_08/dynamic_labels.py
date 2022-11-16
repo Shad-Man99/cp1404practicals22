84f907a6e4f466e22cf384e198f06e7f22ee93cf
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2022-11-16 20:10:54 +1000

    Create a very simple app that has a list of names (strings) and displays each one as a separate Label.

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabels(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Mike", "Stella", "David", "Kate", "Rory", "Alex"]
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file("dynamic_labels.kv")
+        for name in self.names:
+            name_label = Label(text=name)
+            self.root.ids.names_box.add_widget(name_label)
+        return self.root
+
+    def create_name_label(self):
+        name = self.root.ids.name_input.text
+        self.root.ids.names_box.add_widget(Label(text=name))
+        self.root.ids.name_input.text = ""
+
+
+if __name__ == '__main__':
+    DynamicLabels().run()
