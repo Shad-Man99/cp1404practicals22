e8741d8236b7451ccb1eab57a0411b0f64ced6f3
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2022-11-23 21:10:00 +1000

    Add taxi class and tests.

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+Taxi test
+"""
+
+from prac_09.taxi import Taxi
+
+taxi_1 = Taxi("Prius 1", 100)
+taxi_1.drive(40)
+fare = taxi_1.get_fare()
+print(taxi_1)
+print("${:.2f}".format(fare))
+taxi_1.start_fare()
+taxi_1.drive(100)
+fare = taxi_1.get_fare()
+print(taxi_1)
+print("${:.2f}".format(fare))
